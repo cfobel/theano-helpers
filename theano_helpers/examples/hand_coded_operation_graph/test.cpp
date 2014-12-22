1ba0c1f0033d096b653ba3e8e07723f3179ae64d
Christian Fobel <christian@fobel.net>
2014-12-22 10:15:15 -0500

    Update example to `c = (scalar * a) / b`

--- theano_helpers/examples/hand_coded_operation_graph/test.cpp
+++ theano_helpers/examples/hand_coded_operation_graph/test.cpp
@@ -7,16 +7,22 @@
 
 
 int main(int argc, char const* argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: " << argv[0] << " <N>" << std::endl;
+  if (argc != 5) {
+    std::cerr << "usage: " << argv[0] << " <N> scalar a b" << std::endl
+      << std::endl;
+    std::cerr << "computes: " << std::endl << "    (scalar * a) / b"
+      << std::endl;
     exit(-1);
   }
-
-  const int N = atoi(argv[1]);
 
   typedef int A_T;
   typedef float B_T;
   typedef B_T output_T;
+
+  const int N = atoi(argv[1]);
+  const output_T scalar = atof(argv[2]);
+  const A_T a = atof(argv[3]);
+  const B_T b = atof(argv[4]);
 
   typedef typename thrust::device_vector<A_T>::iterator A_iterator;
   typedef typename thrust::device_vector<B_T>::iterator B_iterator;
@@ -25,10 +31,10 @@ int main(int argc, char const* argv[]) {
   thrust::device_vector<B_T> B(N);
   thrust::device_vector<output_T> C(N);
 
-  thrust::fill(A.begin(), A.end(), 1);
-  thrust::fill(B.begin(), B.end(), 2.5);
+  thrust::fill(A.begin(), A.end(), a);
+  thrust::fill(B.begin(), B.end(), b);
 
-  uuid00::Run()(A.begin(), N, B.begin(), C.begin());
+  uuid00::Run()(scalar, A.begin(), N, B.begin(), C.begin());
 
   for (int i = 0; i < N; i++) {
     std::cout << std::setw(4) << C[i];
--- theano_helpers/examples/hand_coded_operation_graph/test.hpp
+++ theano_helpers/examples/hand_coded_operation_graph/test.hpp
@@ -2,6 +2,7 @@
 #define ___TEST__HPP___
 
 #include <thrust/iterator/iterator_traits.h>
+#include <thrust/iterator/constant_iterator.h>
 #include <thrust/copy.h>
 #include "Transform.hpp"
 
@@ -9,20 +10,30 @@
 namespace uuid00 {
   struct Run {
     template <typename A, typename B, typename C>
-    void operator() (A a, size_t N, B b, C c) {
+    void operator() (
+      typename thrust::iterator_traits<C>::value_type scalar,
+      A a, size_t N, B b, C c) {
       /* Operation:
        *
-       *     c = a / b
+       *     c = (scalar * a) / b
        */
-      typedef typename thrust::iterator_traits<C>::value_type result_type;
-      typedef thrust::divides<result_type> Op;
-      typedef theano_helpers::Transform2<Op, A, B> operator_graph_type;
-      typedef typename operator_graph_type::iterator operator_graph_iterator;
+      typedef typename thrust::iterator_traits<C>::value_type result_type1;
+      typedef thrust::divides<result_type1> Op1;
 
-      operator_graph_iterator operator_graph =
-        operator_graph_type(a, b, Op()).begin();
+      typedef typename thrust::iterator_traits<B>::value_type result_type0;
+      typedef thrust::multiplies<result_type0> Op0;
 
-      thrust::copy_n(operator_graph, N, c);
+      typedef theano_helpers::Transform2
+        <Op0, thrust::constant_iterator<result_type0>, A> transform0;
+      typedef typename transform0::iterator iterator0;
+
+      typedef theano_helpers::Transform2<Op1, iterator0, B> transform1;
+      typedef typename transform1::iterator iterator1;
+
+      iterator0 node0 = transform0(thrust::constant_iterator<result_type0>(scalar), a, Op0()).begin();
+      iterator1 node1 = transform1(node0, b, Op1()).begin();
+
+      thrust::copy_n(node1, N, c);
     }
   };
 
